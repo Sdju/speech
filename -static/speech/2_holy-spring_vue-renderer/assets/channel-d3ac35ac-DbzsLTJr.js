f33fee93d616a56b49b8c77cd2d59ada057198cb
zede <zede1697@mail.ru>
2024-04-29 21:40:25 +0500

    github CI

---
+++ -static/speech/2_holy-spring_vue-renderer/assets/channel-d3ac35ac-DbzsLTJr.js
@@ -0,0 +1,1 @@
+import{am as o,an as r}from"./slidev/50-CiAKkEcF.js";const s=(n,a)=>o.lang.round(r.parse(n)[a]),e=s;export{e as c};
--- vue-renderer/slideshow/theme/layouts/cover.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="slidev-layout cover h-full flex">
-    <div class="w-full flex flex-col justify-center">
-      <slot />
-    </div>
-  </div>
-</template>
