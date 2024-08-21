9374f0872bf6879654afbaccc9014d556ed86717
zede <zede1697@mail.ru>
2024-08-21 22:09:48 +0500

    omsk increment

---
+++ 2024/5_omsk_safe-ts/slideshow/theme/components/backgrounds/ParticlesBackground.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {onMounted, ref} from 'vue';
+
+const canvas = ref<HTMLCanvasElement>()
+const points = Array(150).fill(0).map(() => ({
+
+}))
+let ctx: CanvasRenderingContext2D;
+
+onMounted(() => {
+  const canvasEl = canvas.value
+  ctx = canvasEl.getContext('2d')
+
+  canvasEl.width = canvasEl.clientWidth
+  canvasEl.height = canvasEl.clientHeight
+})
+
+// Util
+function getDistance(p1, p2) {
+  return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+}
+
+</script>
+
+<template>
+  <div class="relative">
+    <canvas id="demo-canvas" class="absolute w-full h-full top-0 left-0" />
+  </div>
+</template>
---
+++ 2024/5_omsk_safe-ts/slideshow/theme/layouts/full.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="full w-full h-full flex" >
+    <div class="w-full h-full flex flex-col justify-center items-stretch">
+      <slot />
+    </div>
+  </div>
+</template>
+<style>
+.center {
+  h2 {
+    @apply my-4;
+  }
+}
+</style>
+<script setup>
+</script>
