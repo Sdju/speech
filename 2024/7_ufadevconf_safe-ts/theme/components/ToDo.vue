84be0003e972430c55b1ddddacec2e804e2b58b5
zede <zede1697@mail.ru>
2024-11-09 00:44:12 +0500

    holy increment

--- 2024/7_ufadevconf_safe-ts/theme/components/ToDo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="text-blue text-[4em] text-shadow-lg"> РЕАЛИЗОВАТЬ </div>
-</template>
--- 2024/7_ufadevconf_safe-ts/theme/components/ZedeDither.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import dither from '../image/Dither.png'
-</script>
-
-<template>
-  <div class="absolute top-0 left-0 w-full h-full" :style="{background: `url(${dither})`, opacity: '0.15'}" />
-</template>
